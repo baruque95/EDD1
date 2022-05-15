efb62d14e7f49490187b3befbc7b850dfcaf8fcc
Gabriel Baruque <baruque@edu.unirio.br>
2022-05-15 18:29:57 -0300

    Exercicio2

--- Lista1/Ex1.java
+++ Lista1/Ex1.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class MainPilha {
+public class Ex1 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         Pilha pilha = new Pilha(40);
---
+++ Lista1/Ex2.java
@@ -0,0 +1,69 @@
+import java.util.Scanner;
+
+public class Ex2 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Pilha pilha = new Pilha(40);
+        String s;
+        char c;
+
+        System.out.println("Digite uma combinação de letras.");
+        s = scanner.nextLine();
+        s.toLowerCase();
+
+        int i = 0;
+
+        while (i < s.length()) {
+            c = s.charAt(i);
+            pilha.push(c);
+            i++;
+        }
+
+        isValid(pilha, s);
+        scanner.close();
+    }
+
+    public static boolean isValid(Pilha pilha, String s) {
+        int tam = s.length();
+        int ciclo = defineCiclo(pilha, s);
+        char c;
+
+        if (ciclo == 0) {
+            System.out.println("Falhou... Formatação incorreta.");
+            return false;
+        }
+
+        for (int i = 0; i < tam; i++) {
+            if (i % ciclo == 0) {
+                Pilha pilhaAux = new Pilha(ciclo);
+                c = pilha.pop();
+                if (c == s.charAt(i)) {
+                    pilhaAux.push(c);
+                }
+                for (int j = i; j < ciclo; j++) {
+                    char x = pilhaAux.pop();
+                    if (x != s.charAt(tam - ciclo + 1)) {
+                        System.out.println("Falhou... Formatação incorreta.");
+                        return false;
+                    }
+                }
+            }
+        }
+
+        System.out.println("SUCESSO!! A cadeia de caracteres obedece aos critérios necessários.");
+        return true;
+    }
+
+    public static int defineCiclo(Pilha pilha, String s) {
+        int tam = s.length();
+        int ciclo;
+
+        for (int i = 0; i < tam; i++) {
+            if (s.charAt(i) == 'c' && s.charAt(i * 2 + 1) == 'd') {
+                ciclo = i * 2 + 1;
+                return ciclo;
+            }
+        }
+        return ciclo = 0;
+    }
+}
