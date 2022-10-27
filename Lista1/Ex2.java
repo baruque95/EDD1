24937bac14a4ab30fd6067c7252e8da59024d456
baruque95 <baruque@edu.unirio.br>
2022-10-27 13:04:27 -0300

    mudanças no ex 2

--- Lista1/Ex2.java
+++ Lista1/Ex2.java
@@ -8,7 +8,7 @@ public class Ex2 {
 
         System.out.println("Digite uma combinação de letras.");
         s = scanner.nextLine();
-        s.toLowerCase();
+        s = s.toLowerCase();
 
         int i = 0;
 
@@ -23,47 +23,24 @@ public class Ex2 {
 
     public static boolean isValid(Pilha pilha, String s) {
         int tam = s.length();
-        int ciclo = defineCiclo(pilha, s);
         char c;
 
-        if (ciclo == 0) {
-            System.out.println("Falhou... Formatação incorreta.");
-            return false;
-        }
-
         for (int i = 0; i < tam; i++) {
-            if (i % ciclo == 0) {
-                Pilha pilhaAux = new Pilha(ciclo);
-                c = pilha.pop();
-                if (c == s.charAt(i)) {
-                    pilhaAux.push(c);
+            c = pilha.pop();
+            if (s.charAt(i) == c) {
+                if (i != tam / 2 && c != 'a' && c != 'b') {
+                    System.out.println("Falhou... Algum caractere não-central é diferente de A e B.");
+                    return false;
+                } else if (i == tam / 2 && c != 'c') {
+                    System.out.println("Falhou... O caractere central não é c.");
+                    return false;
                 }
-                for (int j = i; j < ciclo; j++) {
-                    char x = pilhaAux.pop();
-                    if (x != s.charAt(tam - ciclo + 1)) {
-                        System.out.println("Falhou... Formatação incorreta.");
-                        return false;
-                    }
-                }
+            } else {
+                System.out.println("Falhou... A cadeia de caracteres não é um palíndromo.");
+                return false;
             }
         }
-
         System.out.println("SUCESSO!! A cadeia de caracteres obedece aos critérios necessários.");
         return true;
     }
-
-    public static int defineCiclo(Pilha pilha, String s) {
-        int tam = s.length();
-        int ciclo;
-
-        for (int i = 0; i < tam; i++) {
-            if (s.charAt(i) == 'c' && s.charAt(i * 2 + 1) == 'd') {
-                ciclo = i * 2 + 1;
-                return ciclo;
-            }
-        }
-        return ciclo = 0;
-    }
 }
-
-// Não consegui terminar a lógica! Programa não funcionando
